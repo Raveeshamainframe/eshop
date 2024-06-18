fc0c525546cff0f782ab8e7a29b94d31f81c6741
Raveesha Ramitha <ramithamainframe@gmail.com>
2024-06-18 15:29:13 +0530

    first commit

---
+++ footer.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="bootstrap.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <footer class=" bg-dark text-white pt-5 pb-4">
+        <div class="col-12 text-md-start">
+            <div class="row text-sm-center text-md-start">
+
+                <div class="col-12 col-md-3 col-lg-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 text-warning">Company Name</h5>
+                    <p>Here we are the eShop.lk&trade; to support you for accomplish your dessire by selling high quality products. </p>
+                </div>
+                <div class="col-12 col-md-3 col-lg-2 mx-auto mt-3">
+                    <p>Copyright &copy;2021 eShop All Right Reserved</p>
+                </div>
+
+                <div class="col-12 col-md-2 col-lg-2 mx-auto mt-3">
+                    <div class="text-center text-md-start">
+                        <ul class="list-unstyled list-inline">
+
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-facebook" style="font-size: 22px;">
+                                    </i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-twitter" style="font-size: 22px;">
+                                    </i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-google" style="font-size: 22px;">
+                                    </i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-linkedin" style="font-size: 22px;">
+                                    </i>
+                                </a>
+                            </li>
+                            <li class="list-inline-item">
+                                <a href="#" class="form-floating text-white">
+                                    <i class="bi bi-youtube" style="font-size: 22px;">
+                                    </i>
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-4 col-lg-3 mx-auto mt-3">
+                    <h5 class="text-uppercase mb-4 text-warning">Contact</h5>
+                    <p><i class="bi bi-house-door-fill"></i> Maradana, Colombo 10, Sri Lanka</p>
+                    <p><i class="bi bi-envelope-fill"></i> eShop@gmail.com</p>
+                    <p><i class="bi bi-telephone-fill"></i> +94 112 215684</p>
+                    <p><i class="bi bi-printer-fill"></i> +94 112 325478</p>
+                </div>
+
+            </div>
+            <hr class="mb-4" />
+        </div>
+    </footer>
+    <script src="bootstrap.bundle.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+</body>
+
+</html>
---
+++ userprofile.js
@@ -0,0 +1,35 @@
+function updateProfile() {
+
+    var fname = document.getElementById("fname");
+    var lname = document.getElementById("lname");
+    var mobile = document.getElementById("mobile");
+    var line1 = document.getElementById("line1");
+    var line2 = document.getElementById("line2");
+    var city = document.getElementById("city");
+    var img = document.getElementById("profileimg");
+
+    var f = new FormData();
+
+    f.append("f", fname.value);
+    f.append("l", lname.value);
+    f.append("m", mobile.value);
+    f.append("a1", line1.value);
+    f.append("a2", line2.value);
+    f.append("c", city.value);
+    f.append("i", img.files[0]);
+
+    var r = new XMLHttpRequest();
+    r.onreadystatechange = function() {
+        if (r.readyState == 4) {
+            var t = r.responseText;
+            window.location = "userprofile.php";
+            alert(t);
+            // window.location = "userprofile.php";
+        }
+    }
+
+    r.open("POST", "UpdateProfileProcess.php", true);
+    r.send(f);
+
+
+}
